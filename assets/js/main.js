9be4d1db7fb9a7fc5deac23d606e499af07aa710
asadbek <asadbekqulboyev@gmail.com>
2025-12-11 12:26:49 +0500

    finish

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+const slider = new Swiper(".single_slider_swiper", {
+  slidesPerView: 1,
+  loop: true,
+  navigation: {
+    nextEl: ".single_slider_next",
+    prevEl: ".single_slider_prev",
+  },
+});
