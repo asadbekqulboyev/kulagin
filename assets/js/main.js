da6b1a3d795f0227e70a70580015175ff851576b
asadbek <asadbekqulboyev@gmail.com>
2025-12-13 00:12:26 +0500

    finish

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,20 @@ const slider = new Swiper(".single_slider_swiper", {
     prevEl: ".single_slider_prev",
   },
 });
+$(".header__burger").on("click", function () {
+  $(".menu").addClass("open");
+  $("body").addClass("lock");
+});
+
+$(".menu__close").on("click", function () {
+  $(".menu").removeClass("open");
+  $("body").removeClass("lock");
+});
+
+$(document).mouseup(function (e) {
+  var div = $(".menu");
+  if (!div.is(e.target) && div.has(e.target).length === 0) {
+    $(".menu").removeClass("open");
+    $("body").removeClass("lock");
+  }
+});
